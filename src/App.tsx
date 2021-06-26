8c3790fc5b617bc111d7d615c87990177c8056ed
Akin <rapiddevelopa@gmail.com>
2021-06-26 19:11:38 +0100

    exchange-rate-list-added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { createStyles, makeStyles, Theme } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
+
+import TextField from '@material-ui/core/TextField';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import ExchangeRateList from './Components/ExchangeRateList/ExchangeRateList';
 
 const useStyles = makeStyles((theme: Theme)=>(
   createStyles({
@@ -16,13 +19,35 @@ const useStyles = makeStyles((theme: Theme)=>(
        fontSize: 20
      }, 
 
+     root: {
+       flexGrow: 1
+     },
+
      surfaceColor: {
        color: 'white'
      },
 
      outlined: {
        border: '1px solid white'
-     }
+     },
+
+     ctrlsContainer: {
+        marginTop: '64px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 32,
+        formControl: {
+          marginRight: '24px',
+        },
+     },
+
+     textField: {
+       marginRight: 32
+     }, 
+
+
+
   })
 ))
 
@@ -30,9 +55,10 @@ function App() {
 
   const classes = useStyles()
 
+ 
   return (
     <div className="App">
-      <AppBar>
+          <AppBar>
         <Toolbar>
           <Typography variant="subtitle1" classes={{subtitle1: classes.subtitle1}}>
             CurrencyBase
@@ -43,14 +69,14 @@ function App() {
           </Button>
         </Toolbar>
       </AppBar>
-      <div>
-        <div>Textfield</div>
-        <div>Convert Button</div>
-        <div>Add Target Currency Button</div>
+      
+    
+      <div className={classes.ctrlsContainer}>
+         <TextField className={classes.textField} id="outlined-basic"  label="Amount" variant="outlined" />
+         <Button variant="contained">Convert</Button>
+        {/* <div>Add Target Currency Button</div> */}
       </div>
-      <div className={"targetCurrenciesContainer"}>
-        <div>ListItem</div>
-      </div>
+       <ExchangeRateList/>
     </div>
   );
 }
---
+++ src/Components/ExchangeRateList/ExchangeRateList.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
+import List from "@material-ui/core/List";
+
+
+const useStyles = makeStyles((theme: Theme)=>(
+    createStyles({
+       targetCurrenciesContainer: {
+         display: 'grid'
+       }
+  
+    })
+  ))
+
+const ExchangeRateList = ()=>{
+
+    const exchangeRatelist = [1, 2, 3, 4, 5, 6, 7, 8].map((item, index) =>
+        <>
+            <ListItem button>
+                <ListItemText primary="NAIRA" secondary="NGN" />
+                <Typography variant="h6">
+                    $200.00
+                </Typography>
+            </ListItem>
+            <Divider variant="inset" />
+        </>
+    )
+
+
+    return (
+        <List>
+            {exchangeRatelist}
+        </List>
+    )
+
+}
+
+export default ExchangeRateList
