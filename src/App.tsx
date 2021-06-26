87241f0f95983b2c567492b6e613794e8cdc7156
Akin <rapiddevelopa@gmail.com>
2021-06-26 15:24:17 +0100

    structure-added-for-exchange-screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>AppBar</div>
+      <div>
+        <div>Textfield</div>
+        <div>Convert Button</div>
+        <div>Add Target Currency Button</div>
+      </div>
+      <div className={"targetCurrenciesContainer"}>
+        <div>ListItem</div>
+      </div>
     </div>
   );
 }
